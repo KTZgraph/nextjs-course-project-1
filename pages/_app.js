15a27cc9680a1d848e22e3acef84ff07f44082fe
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-06 20:17:45 +0100

    77 dodawanie layoutu w tym menu i logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      {/* można tak opakowac, bo layout też przyjmuje props.children */}
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
