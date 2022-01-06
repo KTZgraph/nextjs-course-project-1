ba9a40ef0e1cecde41fa1da5b6c0410a5c967c7c
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-06 18:54:29 +0100

    tyle z 03-prj-routing/s

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,40 +1,42 @@
 import Link from "next/link";
 
-function EventItem(props){
-    const {title, image, date, location, id} = props;
+function EventItem(props) {
+  const { title, image, date, location, id } = props;
 
-    // https://developer.mozilla.org/pl/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
-    const humanReadableData = new Date(date).toLocaleDateString('en-US', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-    });
+  // https://developer.mozilla.org/pl/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+  const humanReadableData = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 
-    // string replace bo ulica i miasto oddzielone przecinkiem
-    const formattedAddress = location.replace(', ', '\n')
+  // string replace bo ulica i miasto oddzielone przecinkiem
+  const formattedAddress = location.replace(", ", "\n");
 
-    // konstrukcja linku do konkretnego wydarzenia po id
-    const exploreLink = `/events/${id}`;
+  // konstrukcja linku do konkretnego wydarzenia po id
+  const exploreLink = `/events/${id}`;
 
-    // dla konkrentych danych
-    return <l1>
-        {/* nie trzeba dodawać publixc na początek, bo folder public jest dostepny wszedzie */}
-        <img src={'/' + image} alt={title} />
+  // dla konkrentych danych
+  return (
+    <l1>
+      {/* nie trzeba dodawać publixc na początek, bo folder public jest dostepny wszedzie */}
+      <img src={"/" + image} alt={title} />
+      <div>
         <div>
-            <div>
-                <h2>{title}</h2>
-                <div>
-                    <time>{humanReadableData}</time>
-                </div>
-                <div>
-                    <address>{formattedAddress}</address>
-                </div>
-            </div>
-            <div>
-                <Link href={exploreLink}>Explore Event</Link>
-            </div>
+          <h2>{title}</h2>
+          <div>
+            <time>{humanReadableData}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
         </div>
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
     </l1>
+  );
 }
 
 export default EventItem;
