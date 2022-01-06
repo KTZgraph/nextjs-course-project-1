2533a399c785645802f0e162fb860c377b8bc9b6
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-06 20:03:48 +0100

    76 szczególły dla pojedycnzego wydarzenia

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,12 +1,20 @@
 import { useRouter } from "next/router";
+import { Fragment } from "react";
 
-import getEventById from "../../dummy-data";
+import {getEventById} from "../../dummy-data"; // pamietac, że to nie jedyna eksportowana funckja!
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 function EventDetailPage() {
   const router = useRouter();
 
   const eventId = router.query.eventId;
+  console.log("\n\n\n\n\n")
+  console.log("eventId: ", eventId)
   const event = getEventById(eventId);
+  console.log("event: ", event)
+
 
   // walidacja danych
   if (!event) {
@@ -14,9 +22,18 @@ function EventDetailPage() {
   }
 
   return (
-    <div>
-      <h1> Event Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 
