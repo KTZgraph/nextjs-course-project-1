9244d407749b416f1934057cd14957e5e67468ce
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-06 15:23:21 +0100

    70 koniec

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+// dla dwóch i wiecej dynamicznych parametrów wejdzie tutaj a nie do [eventId].js
+
+function FilteredEventsPage() {
+  return (
+    <div>
+      <h1> Filtered Events</h1>
+    </div>
+  );
+}
+
+export default FilteredEventsPage;
