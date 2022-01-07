6250e7b294e36773866e6cbe27188747f10f8dce
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-07 22:12:50 +0100

    pobieranie po stronie klienta

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,34 +1,59 @@
 // dla dwóch i wiecej dynamicznych parametrów wejdzie tutaj a nie do [eventId].js
 import { useRouter } from "next/router"; // z nexta
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import useSWR from "swr";
 
-import {getFilteredEvents} from "../../helpers/api-utils";
+import { getFilteredEvents } from "../../helpers/api-utils";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
 
 function FilteredEventsPage(props) {
+  const [loadedEvents, setLoadedEvents] = useState();
+  //potrzebne dane do pobierania dancyh po stronie klienta
+  const router = useRouter();
 
+  const filterData = router.query.slug; // dopiero jak się komponent wyrenderuje - wiec 2 razy sie wykonuje, nie problem ale trzeba sprawdzić czy mam yjuż dane
 
-  //teraz już nie trzeba filtrować danych z urla - mamy to w getServerSideProps
-  // const router = useRouter();
+  const { data, error } = useSWR(
+    "https://nextjs-course-28060-default-rtdb.firebaseio.com/events"
+  );
 
-  // const filterData = router.query.slug; // dopiero jak się komponent wyrenderuje - wiec 2 razy sie wykonuje, nie problem ale trzeba sprawdzić czy mam yjuż dane
+  useEffect(() => {
+    if (data) {
+      //transformacja danych
+      const events = [];
+      for (const key in data) {
+        // spread operator ...data[key] żeby nie przepisywać wszystkich kluczy z bazy recznie
+        events.push({ id: key, ...data[key] });
+      }
 
-  // if (!filterData) {
-  //   // globalnie dostepny css
-  //   return <p className="center">Loading...</p>;
-  // }
+      setLoadedEvents(events);
+    }
+  }, [data]);
 
-  // const filteredYear = filterData[0]; // zawsze string
-  // const filteredMonth = filterData[1];
+  if (!loadedEvents) {
+    return <p className="center">Loading...</p>;
+  }
 
-  // const numYear = +filteredYear; // zamiana na liczbę przez znak dodawania JS triczek
-  // const numMonth = +filteredMonth;
+  const filteredYear = filterData[0]; // zawsze string
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear; // zamiana na liczbę przez znak dodawania JS triczek
+  const numMonth = +filteredMonth;
+
+  //trochę skrót - noramlnei chcemy pobrac tylko te dane któe spełniają filtry
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return eventDate.getFullYear() === numYear && eventDate.getMonth() === numMonth - 1;
+  });
+
+  // pobieranie po stronie klienta
 
   // walidacja czy dane z urla prawdiłowe, czy n. ktoś nie wpisał abss zamiast liczby
-  if (props.hasError) { // gdy mamy konkrentgo propsa, ze jest błąd
+  if (props.hasError) { //już teraz nie będzie działać bo nie używamy renderowania po strownie serwera tylko klienta
+    // gdy mamy konkrentgo propsa, ze jest błąd
     return (
       <Fragment>
         <ErrorAlert>
@@ -70,7 +95,8 @@ function FilteredEventsPage(props) {
   );
 }
 
-export async function getServerSideProps(context) { // to jest ok, ale z pobieraniem po stronie klienta dane będą się pobierać trochę szybciej
+export async function getServerSideProps(context) {
+  // to jest ok, ale z pobieraniem po stronie klienta dane będą się pobierać trochę szybciej
   // i ten widok nie jest tak ważny dla search engines
   const { params } = context;
 
@@ -93,7 +119,7 @@ export async function getServerSideProps(context) { // to jest ok, ale z pobiera
   ) {
     //gdy nie udało się pobrac danych z filtra
     return {
-      props: {hasError: true}, // konkrenty komunikat błedu
+      props: { hasError: true }, // konkrenty komunikat błedu
       // notFound: true, // 404
       // redirect: { // przekirowanie do jakieś strony
       //   destination:'/error'
@@ -113,8 +139,8 @@ export async function getServerSideProps(context) { // to jest ok, ale z pobiera
       // do naglowka w wyfiltrowanych danych
       date: {
         year: numYear,
-        month: numMonth
-      }
+        month: numMonth,
+      },
     },
   };
 }
