eec18a29a154ecb0dd7148cb988c874c8afa3ca8
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-08 14:01:16 +0100

    131 general Head

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       {/* head tagi dla wszsytkich stron, żeby nie kopiować w komponentach */}
+      {/* nextjs merguje Head z komponentow gdy nie ma konfliktów, przy konfliktach np title to bierze ostatni */}
       <Head>
+        {/* ponizej tytuł, jak zapomne dodac title w komponencie to bedzie ten, a jak nie to nadpisany z komponentu */}
+        <title>Next Events</title>
+        <meta name="description" content=""/>
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
       </Head>
       {/* można tak opakowac, bo layout też przyjmuje props.children */}
