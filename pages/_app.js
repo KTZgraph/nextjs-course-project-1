64cd8d97ee1999cad8509e66e6c51ddad4288358
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-01-08 13:56:54 +0100

    130 Head dla wszystkich stron "viewport" content=

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import Head from "next/head";
+
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      {/* head tagi dla wszsytkich stron, żeby nie kopiować w komponentach */}
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      </Head>
       {/* można tak opakowac, bo layout też przyjmuje props.children */}
       <Component {...pageProps} />
     </Layout>
